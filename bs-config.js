dbf6faeac91673ea8ddb699b3b15a9a9491c7d3b
Oshima Daiki <73574599+oshima-daiki@users.noreply.github.com>
2022-05-23 14:23:36 +0900

    Initial commit

---
+++ bs-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	"files": "./dist/**/*.css, ./dist/**/*.js, ./dist/**/*.html",
+    "server": {
+        baseDir: 'dist',
+        index: 'index.html',
+    },
+    "proxy": false,
+    "port": 3000
+};
