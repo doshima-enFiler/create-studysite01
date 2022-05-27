5b6f519d06a7b182886817f2f31ee990b2de330c
enCreate-od <daikougun0923@gmail.com>
2022-05-28 07:01:44 +0900

    edit hamburger-menu

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,6 @@ const hamburger = document.getElementById('jsHamburger');
 const hamburgerLine = document.querySelectorAll('.l-header__hamburger-line');
 const headerNav = document.getElementById('jsNav');
 
-const headerHeight = 60 //ヘッダーの高さ
-let bodyHeight //ウィンドウの高さを入れる場所
-let scrollpos　//スクロールの位置を入れる場所
-
 hamburger.addEventListener('click', function () {
   // ハンバーガーボタンを変形
   for (let i = 0; i < hamburgerLine.length; i++) {
